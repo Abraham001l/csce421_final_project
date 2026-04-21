0955039f4c0b75a8e9ce4aefa7171b6848b9318a
abraham001l <abraham51lbucio@gmail.com>
2026-04-20 23:05:57 -0500

    adding results folder

--- project/models/helper_code/mimic_dataset.py
+++ project/models/helper_code/mimic_dataset.py
@@ -1,17 +1,26 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
+from transformers import AutoTokenizer
 
 class mimic_dataset(Dataset):
-    def __init__(self, text, labels):
+    def __init__(self, text, labels, tokenizer, device=None):
         self.text = text
         self.labels = labels
+        self.tokenizer = tokenizer
+        self.device = device
 
-        # printing shapes for debugging
-        print(f"shape of label: {torch.tensor(self.labels[0], dtype=torch.float32).shape}")
-
-    
     def __len__(self):
         return len(self.text)
     
     def __getitem__(self, idx):
-        return self.text[idx], torch.tensor(self.labels[idx], dtype=torch.float32)
+        text = self.text[idx]
+
+        encoding = self.tokenizer(text, 
+                                  padding='max_length', 
+                                  truncation=True, 
+                                  return_tensors='pt', 
+                                  max_length=512)
+        
+        item = {key: val.squeeze(0) for key, val in encoding.items()}
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
+        return item, label
