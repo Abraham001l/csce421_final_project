efdd9b1bb6d5355b6756b0ee8fdca2445633240b
abraham001l <abraham51lbucio@gmail.com>
2026-04-26 11:28:48 -0500

    optimized mimic_dataset to not double cast to tensor when using precomputed labels

--- project/models/helper_code/mimic_dataset.py
+++ project/models/helper_code/mimic_dataset.py
@@ -13,18 +13,19 @@ class mimic_dataset(Dataset):
         return len(self.text)
     
     def __getitem__(self, idx):
-        label = torch.tensor(self.labels[idx], dtype=torch.float32)
-
         if self.precomputed:
+            label = self.labels[idx].clone().detach().float()
             item = self.text[idx].clone().detach()
             return item, label
-        
-        text = self.text[idx]
-        encoding = self.tokenizer(text, 
-                                  padding='max_length', 
-                                  truncation=True, 
-                                  return_tensors='pt', 
-                                  max_length=512)
-        item = {key: val.squeeze(0) for key, val in encoding.items()}
-        
-        return item, label
+        else:
+            label = torch.tensor(self.labels[idx], dtype=torch.float32)
+            
+            text = self.text[idx]
+            encoding = self.tokenizer(text, 
+                                    padding='max_length', 
+                                    truncation=True, 
+                                    return_tensors='pt', 
+                                    max_length=512)
+            item = {key: val.squeeze(0) for key, val in encoding.items()}
+            
+            return item, label
